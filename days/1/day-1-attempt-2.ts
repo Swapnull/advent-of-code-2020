a43375e511dd0d561b3f08f10bfc5e9c7aa3e6d1
Martyn Rushton <martyn.e.rushton@gmail.com>
2020-12-01 23:01:56 +0000

    feat(day-1): add second attempt at day 1. Should be a computationally better solution

---
+++ days/1/day-1-attempt-2.ts
@@ -0,0 +1,38 @@
+import inputList from "./day-1-input";
+
+const part1 = (goal = 2020) => {
+  let res: number = 0;
+
+  inputList.every((first: number) => {
+    const remainder = goal - first;
+    const foundSecond = inputList.find(
+      (item: number) => item === remainder && item
+    );
+    if (foundSecond) {
+      res = foundSecond * first;
+      return false;
+    }
+    return true;
+  });
+
+  return res;
+};
+console.log(part1());
+
+const part2 = (goal = 2020) => {
+  let res: number = 0;
+
+  inputList.every((first: number) => {
+    const remainder = goal - first;
+    const rest = part1(remainder);
+    if (rest) {
+      res = first * rest;
+      return false;
+    }
+    return true;
+  });
+
+  return res;
+};
+
+console.log(part2());

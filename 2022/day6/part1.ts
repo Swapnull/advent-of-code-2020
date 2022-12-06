680e927d1b4d000c01649768c86e80d81e7a65c5
Swapnull <martyn.e.rushton@gmail.com>
2022-12-06 14:12:11 +0000

    day6

---
+++ 2022/day6/part1.ts
@@ -0,0 +1,17 @@
+import fs from "fs";
+import { uniq } from "lodash";
+
+const input = fs.readFileSync("input.txt", "utf-8").split("");
+
+let curr = [];
+const startPoint = 4;
+
+for (let i = 0; i < input.length; i++) {
+  const char = input[i];
+  if (curr.length < startPoint) {
+    curr = [...curr, char];
+  } else if (uniq(curr).length === startPoint) {
+    console.log("found", i);
+    break;
+  } else curr = [...curr.slice(1), char];
+}

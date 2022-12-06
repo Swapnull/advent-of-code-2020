b7ef65a3affc64c298d9699d50a963306d165b59
Swapnull <martyn.e.rushton@gmail.com>
2022-12-06 14:18:32 +0000

    day6 improvements

--- 2022/day6/part1.ts
+++ 2022/day6/part1.ts
@@ -3,15 +3,24 @@ import { uniq } from "lodash";
 
 const input = fs.readFileSync("input.txt", "utf-8").split("");
 
-let curr = [];
 const startPoint = 4;
 
 for (let i = 0; i < input.length; i++) {
-  const char = input[i];
-  if (curr.length < startPoint) {
-    curr = [...curr, char];
-  } else if (uniq(curr).length === startPoint) {
-    console.log("found", i);
+  if (uniq(input.slice(i, i + startPoint)).length === startPoint) {
+    console.log("found", i + startPoint);
     break;
-  } else curr = [...curr.slice(1), char];
+  }
 }
+
+/* original idea, worked but more effort*/
+
+// let curr = []
+// for (let i = 0; i < input.length; i++) {
+//   const char = input[i];
+//   if (curr.length < startPoint) {
+//     curr = [...curr, char];
+//   } else if (uniq(curr).length === startPoint) {
+//     console.log("found", i);
+//     break;
+//   } else curr = [...curr.slice(1), char];
+// }
--- 2022/day6/part2.ts
+++ 2022/day6/part2.ts
@@ -3,15 +3,11 @@ import { uniq } from "lodash";
 
 const input = fs.readFileSync("input.txt", "utf-8").split("");
 
-let curr = [];
 const startPoint = 14;
 
 for (let i = 0; i < input.length; i++) {
-  const char = input[i];
-  if (curr.length < startPoint) {
-    curr = [...curr, char];
-  } else if (uniq(curr).length === startPoint) {
-    console.log("found", i);
+  if (uniq(input.slice(i, i + startPoint)).length === startPoint) {
+    console.log("found", i + startPoint);
     break;
-  } else curr = [...curr.slice(1), char];
+  }
 }

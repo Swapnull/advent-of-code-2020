95c1c5fcac6f16da100eb2aea88fabb7d530d5e1
Swapnull <martyn.e.rushton@gmail.com>
2022-12-08 12:43:31 +0000

    day8 (and non-completed day 7)

---
+++ 2022/day8/part1.ts
@@ -0,0 +1,64 @@
+import fs from "fs";
+import { isNaN, size, sum, uniq } from "lodash";
+
+const input = fs
+  .readFileSync("input.txt", "utf-8")
+  .split("\n")
+  .map((row) => row.split("").map(Number));
+
+const res = [];
+
+input.map((row, rowIndex) => {
+  res.push([]);
+  row.map((col, colIndex) => {
+    res[rowIndex][colIndex] = false;
+    if (
+      rowIndex === 0 ||
+      colIndex === 0 ||
+      rowIndex === input.length - 1 ||
+      colIndex === row.length - 1
+    ) {
+      res[rowIndex][colIndex] = true;
+    } else {
+      let left = 0;
+      for (let c = 0; c < colIndex; c++) {
+        if (input[rowIndex][c] < col) {
+          left++;
+        }
+      }
+
+      let right = 0;
+      for (let c = colIndex; c < row.length; c++) {
+        if (input[rowIndex][c] < col) {
+          right++;
+        }
+      }
+
+      let top = 0;
+      for (let r = 0; r < rowIndex; r++) {
+        if (input[r][colIndex] < col) {
+          top++;
+        }
+      }
+
+      let bottom = 0;
+      for (let r = rowIndex; r < input.length; r++) {
+        if (input[r][colIndex] < col) {
+          bottom++;
+        }
+      }
+
+      const visibleFromLeft = left >= colIndex;
+      const visibleFromTop = top >= rowIndex;
+      const visibleFromRight = right >= row.length - 1 - colIndex;
+      const visibleFromBottom = bottom >= input.length - 1 - rowIndex;
+
+      res[rowIndex][colIndex] =
+        visibleFromLeft || visibleFromTop || visibleFromRight || visibleFromBottom;
+    }
+  });
+});
+
+// console.log(res);
+
+console.log(res.flat().filter((item) => item).length);
---
+++ 2022/day8/part2.ts
@@ -0,0 +1,72 @@
+import fs from "fs";
+import { isNaN, max, size, sum, uniq } from "lodash";
+
+const input = fs
+  .readFileSync("input.txt", "utf-8")
+  .split("\n")
+  .map((row) => row.split("").map(Number));
+
+const res = [];
+
+input.map((row, rowIndex) => {
+  res.push([]);
+  row.map((col, colIndex) => {
+    res[rowIndex][colIndex] = 0;
+
+    if (
+      rowIndex === 0 ||
+      colIndex === 0 ||
+      rowIndex === input.length - 1 ||
+      colIndex === row.length - 1
+    ) {
+      res[rowIndex][colIndex] = 0;
+    } else {
+      let top = 0;
+      for (let r = rowIndex - 1; r >= 0; r--) {
+        if (input[r][colIndex] < col) {
+          top++;
+        } else {
+          top++;
+          break;
+        }
+      }
+
+      let right = 0;
+      for (let c = colIndex + 1; c < row.length; c++) {
+        if (input[rowIndex][c] < col) {
+          right++;
+          if (input[rowIndex][c] === col) break;
+        } else {
+          right++;
+          break;
+        }
+      }
+
+      let bottom = 0;
+      for (let r = rowIndex + 1; r < input.length; r++) {
+        if (input[r][colIndex] < col) {
+          bottom++;
+        } else {
+          bottom++;
+          break;
+        }
+      }
+
+      let left = 0;
+      for (let c = colIndex - 1; c >= 0; c--) {
+        if (input[rowIndex][c] < col) {
+          left++;
+        } else {
+          left++;
+          break;
+        }
+      }
+
+      res[rowIndex][colIndex] = top * right * bottom * left;
+    }
+  });
+});
+
+// console.log(res);
+
+console.log(max(res.flat()));
